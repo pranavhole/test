2c6b1d3e23d13f85fd13d2df8f331b5ae54059b1
Pranav Hole <85498337+pranavhole@users.noreply.github.com>
2023-08-13 21:33:05 +0530

    first commit

--- src/components/hero.js
+++ src/components/hero.js
@@ -5,7 +5,7 @@ import { fadeIn } from "../utils/motion";
 
 const Hero = () => {
     return(
-     <><motion.section className=" h-screen py-20" variants={textVariant()}><motion.div variants={textVariant()}>
+     <><motion.section className=" h-screen py-20" variants={fadeIn()}><motion.div variants={textVariant()}>
       
         <div className=" text-white z-90 h-full flex flex-col justify-between items-center pt-10 relative title">
           <div className="opacity-100 transform translate-x-0 translate-y-0 translate-z-0 scale-100 rotate-x-0 rotate-y-0 rotate-z-0 skew-x-0 flex flex-col items-center skew-y-0 transform-style-preserve-3d inline-block welcome"><span className="text-3"> Welcome</span><span className="text-3">To AntSwap</span> <span className="descri">Developing a decentralized ecosystem and community Based on Linea Chain.</span></div>
