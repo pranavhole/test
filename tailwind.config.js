69a7045dac8b02d231e2f7fad02c4ec989f949df
Pranav Hole <85498337+pranavhole@users.noreply.github.com>
2023-08-13 21:39:49 +0530

    first commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -22,7 +22,7 @@ module.exports = {
     },
   },
   plugins: [require('postcss-import'),
-  require('@tailwindcss/nesting')(require('postcss-nesting')),
+  require('@tailwindcss/nesting'),require('postcss-nesting'),
   require('tailwindcss'),
   require('autoprefixer'),],
 };
