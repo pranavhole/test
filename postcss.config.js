3659b22fdc9954b7c469e86af0e8904a775ec77e
Pranav Hole <85498337+pranavhole@users.noreply.github.com>
2023-08-13 23:06:42 +0530

    first commit

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,9 @@
 module.exports = {
     plugins: {
-      'postcss-import': {},
-      'tailwindcss/nesting': 'postcss-nesting',
-      tailwindcss: {},
-      autoprefixer: {},
+        'postcss-nested': {before},
+        'postcss-import': {},
+        'tailwindcss/nesting': 'postcss-nesting',
+        tailwindcss: {},
+        autoprefixer: {},
     }
   }
